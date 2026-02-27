a65f353f77921784fb393bae5ab075d68519016a
Syed Hassan <syedhassanshah154@gmail.com>
2026-02-27 21:20:43 +0500

    Update multer.js

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,21 +1,7 @@
 import multer from "multer";
-import { v2 as cloudinary } from "cloudinary";
-import { CloudinaryStorage } from "multer-storage-cloudinary";
 
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
-
-const storage = new CloudinaryStorage({
-  cloudinary: cloudinary,
-  params: {
-    folder: "uploads",
-    format: async (req, file) => "png",
-    public_id: (req, file) => file.originalname,
-  },
-});
+const storage = multer.memoryStorage();
 
 const upload = multer({ storage });
+
 export default upload;
